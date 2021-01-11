2e0c0a629c75f4996020a4420fda711de39e73d4
Harshil <harshilsheth181888@gmail.com>
2021-01-11 13:03:31 +0530

    UnitTest Added

---
+++ Product Management Assignment/PMSTests/ProductsControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMS.Controllers;
+using System.Web.Mvc;
+using System.Linq;
+using System;
+using PMS;
+using PMS.Models;
+
+namespace PMSTests
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+
+
+            //Arrange
+            ProductsController controller = new ProductsController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+        [TestMethod]
+        public void Details()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+
+            //Arrange
+            //  ProductsController controller = new ProductsController();
+
+            //Act
+            var result = _context.Products.ToList();
+
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
---
+++ Product Management Assignment/PMSTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PMSTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PMSTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80368dfc-b983-4d5e-8131-6ace436033f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
